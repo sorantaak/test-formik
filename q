dcb65496845b175acf5945c4af546120267970fe
soran <soran.taak@gmail.com>
2022-10-28 19:36:44 +0330

    create from

---
+++ q
@@ -0,0 +1,11 @@
+[33mcommit ffed33a90ad0eacf16dcde298d0c951d5d6b3870[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: soran <soran.taak@gmail.com>
+Date:   Fri Oct 28 19:26:23 2022 +0330
+
+    install formik and create basic form
+
+[33mcommit af40a44adc5d2ee8fed5ec63e8c81edf722be14b[m
+Author: soran <soran.taak@gmail.com>
+Date:   Fri Oct 28 19:01:13 2022 +0330
+
+    Initialize project using Create React App
--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 function App() {
 	const formik = useFormik({
 		initialValues: {
+			name: "",
+			username: "",
 			email: "",
+			password: "",
 		},
 		onSubmit: (values) => {
-			console.log(values);
+			console.log(JSON.stringify(values));
 		},
 	});
 
@@ -19,10 +22,37 @@ function App() {
 		<div className="App">
 			<form onSubmit={formik.handleSubmit}>
 				<div>
+					<label>Your Name</label>
+					<input
+						name="name"
+						value={formik.values.name}
+						onChange={formik.handleChange}
+						type="text"
+					/>
+				</div>
+				<div>
+					<label>User Name</label>
+					<input
+						name="username"
+						value={formik.values.username}
+						onChange={formik.handleChange}
+						type="text"
+					/>
+				</div>
+				<div>
 					<label>Email Address</label>
 					<input
 						name="email"
 						value={formik.values.email}
+						onChange={formik.handleChange}
+						type="text"
+					/>
+				</div>
+				<div>
+					<label>Pssword</label>
+					<input
+						name="password"
+						value={formik.values.password}
 						onChange={formik.handleChange}
 						type="text"
 					/>
